d3d0cd4c1d7d8644b62d5271a60c465e42fc23e2
Luis Manuel <luismanuelchazy@gmail.com>
2019-01-31 18:42:50 +0100

    added vPoints to graph non ranged x values

--- components/cPath.vue
+++ components/cPath.vue
@@ -14,13 +14,17 @@
 export default {
   name: 'cPath',
   props: {
-    points: Array,
+    hPoints: {
+      type: Array,
+      required: true
+    },
+    vPoints: Array,
     height: {
-      type: Number,
+      type: [Number, String],
       required: true
     },
     width: {
-      type: Number,
+      type: [Number, String],
       required: true
     },
     color: {
@@ -40,16 +44,17 @@ export default {
   },
   computed: {
     pointsResult () {
-      if (this.points && this.points.length > 0) {
-        const minY = this.rangeY ? this.rangeY[0] : this.$utils.getMin(this.points)
-        const maxY = this.rangeY ? this.rangeY[1] : this.$utils.getMax(this.points)
-        const minX = this.rangeX ? this.rangeX[0] : this.$utils.getMin(this.points)
-        const maxX = this.rangeX ? this.rangeX[1] : this.$utils.getMax(this.points)
+      if (this.hPoints && this.hPoints.length > 0) {
+        const minY = this.rangeY ? this.rangeY[0] : this.$utils.getMin(this.hPoints)
+        const maxY = this.rangeY ? this.rangeY[1] : this.$utils.getMax(this.hPoints)
+        const minX = this.rangeX ? this.rangeX[0] : this.$utils.getMin(this.hPoints)
+        const maxX = this.rangeX ? this.rangeX[1] : this.$utils.getMax(this.hPoints)
         let xPoints = []
         let yPoints = []
-        for (let i = 0; i < this.points.length; i++) {
-          const pX = this.$utils.scale(i, minX, maxX, this.width)
-          const pY = this.$utils.scale(this.points[i], minY, maxY, this.height, true)
+        for (let i = 0; i < this.hPoints.length; i++) {
+          const xToScale = this.vPoints ? this.vPoints[i] : i
+          const pX = this.$utils.scale(xToScale, minX, maxX, this.width)
+          const pY = this.$utils.scale(this.hPoints[i], minY, maxY, this.height, true)
           xPoints.push(pX)
           yPoints.push(pY)
         }
